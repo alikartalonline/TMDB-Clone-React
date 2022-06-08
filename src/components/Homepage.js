a35e2b80f3b417e7ccb7bb460e9885fb91ffe2cd
Ali Kartal <ali_kartal91@hotmail.com>
2022-06-08 19:31:28 +0300

    "nav.js" and "section.js" added

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Header from './Header.js';
-import Footer from './Footer.js';
+// import Footer from './Footer.js';
+import Nav from './Nav.js';
 import Section from './Section.js';
-
 
 function Homepage() {
 
@@ -12,10 +12,10 @@ function Homepage() {
     return (
         <div>
             <Header />
+            <Nav />
             <Section />
 
-
-            <Footer />
+            {/* <Footer /> */}
         </div>
     )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,56 +1,157 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import './Section.css'
 
+
+
 function Section() {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        axios("https://api.themoviedb.org/3/movie/popular?api_key=ebac3bbaf26b83349c6f0cda7ed8642c&language=en-US&page=1#")
+            .then(res => setMovies(res.data.results))
+            .catch(e => console.log("error:", e))
+    }, [])
+
+
+
+
     return (
-        <div className=''>
+        <div>
+            <div className='container mt-4'>
 
-            <div className="container">
 
-
-                {/* nav start  */}
                 <div className='row'>
-                    <div className='col-md-12 sectionbackground '>
-
-                        <div className='sectiontitle'>
-                            <h1 className='sectiontitleh'>Hoş Geldiniz.</h1>
-                            <h3 className='sectiontitleh2'>Milyonlarca film, TV şovu ve keşfedilecek kişi. Şimdi keşfedin.</h3>
+                    <div className='d-flex'>
+                        <div className='col-md'>
+                            <h2 className='titlesection text-dark '>Popüler Olanlar</h2>
                         </div>
 
-                        <div className='searchbar'>
-                            <form
-                                className="inner_search_form"
-                                action="/"
-                                acceptCharset='utf-8'
-                            >
+                        <div className='selector col-md'>
+                            <div className='anchor selected'>
+                                <h3 className='selectortitle'>
+                                    <a href="#" className='selectora'>
+                                        Yayın Akışı
+                                    </a>
+                                    <div
+                                        className='background'
+                                    ></div>
+                                </h3>
+                            </div>
 
-                                <input
-                                    type="text"
-                                    className='inner_search_v4'
-                                    dir='auto'
-                                    name="query"
-                                    tabIndex="1"
-                                    autoCorrect='off'
-                                    spellCheck="false"
-                                    placeholder='Film, dizi, kişi ara...'
-                                />
+                            <div className='anchor'>
+                                <h3 className='selectortitle'>
+                                    <a href="#" className='selectora'>
+                                        Televizyonda
+                                    </a>
+                                    <div
+                                        className='background hide'
+                                    ></div>
+                                </h3>
+                            </div>
 
-                                <input
-                                    type="submit"
-                                    value="Search"
-                                    className='searchbutton'
-                                />
+                            <div className='anchor'>
+                                <h3 className='selectortitle'>
+                                    <a href="#" className='selectora'>
+                                        Kiralık
+                                    </a>
+                                    <div
+                                        className='background hide'
+                                    ></div>
+                                </h3>
+                            </div>
 
-                            </form>
+
+                            <div className='anchor'>
+                                <h3 className='selectortitle'>
+                                    <a href="#" className='selectora'>
+                                        Sinemalarda                                    </a>
+                                    <div
+                                        className='background hide'
+                                    ></div>
+                                </h3>
+                            </div>
+
+                        </div>
+
+
+                        <div className='col-md'>
+                        </div>
+                        <div className='col-md'>
+                        </div>
+                        <div className='col-md'>
                         </div>
                     </div>
                 </div>
-                        {/* nav finish  */}
+
+                <section className='row ' >
+                    <div className='scroller '>
+
+                    {
+                        movies.map((item, key) => (
+                            <div className="card mt-3 col-sm-2 border-0  sectioncard" key={item.id}>
+
+                                {/* card img  */}
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${item.poster_path}`}
+                                    className="card-img-top sectionimg"
+                                    alt="card img"
+                                />
+
+                                {/* score bar */}
+                                <div className="progress"
+                                // style={{ width: "100%" }}
+                                >
+                                    <div
+                                        className={
+                                        item.vote_average > 0 && item.vote_average < 2.5 ? "progress-bar bg-danger"
+                                        : item.vote_average > 2.5 && item.vote_average < 5.0 ? "progress-bar bg-warning"
+                                        : item.vote_average > 5.0 && item.vote_average < 7.5 ? "progress-bar bg-info"
+                                        : item.vote_average > 7.5 && item.vote_average < 10.0 ? "progress-bar bg-success"
+                                        : ""}
+
+                                        role="progressbar"
+
+                                        style={{
+                                            width: `${item.vote_average > 0 && item.vote_average < 2.5 ? "25%"
+                                                : item.vote_average > 2.5 && item.vote_average < 5.0 ? "50%"
+                                                : item.vote_average > 5.0 && item.vote_average < 7.5 ? "75%"
+                                                : item.vote_average > 7.5 && item.vote_average < 10.0 ? "100%"
+                                                : ""}`
+                                        }}
+                                    >
+                                    </div>
+                                </div>
+
+                                {/* score %  */}
+                                <div className='contenticon '>
+                                    <div className='consensus tight '>
+                                        <div className='outer_ring'>
+                                            <div className='user_score_chart '>
+                                                <div className='percent'>
+                                                    <span className='icon'>
+                                                        {Math.round(item.vote_average)}0
+                                                    </span>
+                                                    <div className='canvas' >%</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {/* card title and date */}
+                                <h3 className="card-title cardbody"><strong>{item.title}</strong></h3>
+                                <p className="card-text cardbody fw-light">{item.release_date}</p>
+
+                            </div>
+                        ))
+                    }
+                    </div>
+
+                </section>
 
 
-                {/* section start  */}
-                
-                {/* section finish  */}
 
 
 
